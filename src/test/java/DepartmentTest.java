291e5def8c98740674f9da2cefb7a0f167f3c91a
MahmoudAlSaffarini <6297190@edu.vaniercollege.qc.ca>
2025-12-09 18:16:56 -0500

    Commit DepartmentTest Class

---
+++ src/test/java/DepartmentTest.java
@@ -0,0 +1,18 @@
+import org.Mahmoud.Department;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DepartmentTest {
+
+    @Test
+    public void testDepartment() {
+        assertTrue(Department.isDepartmentNameValid("Computer Science and Mathematics"));
+        assertTrue(Department.isDepartmentNameValid("General Chemistry"));
+        assertFalse(Department.isDepartmentNameValid(""));
+        assertFalse(Department.isDepartmentNameValid(null));
+        assertFalse(Department.isDepartmentNameValid("Calculus 1"));
+        assertFalse(Department.isDepartmentNameValid("Computer!Science!and!Mathematics"));
+    }
+}
